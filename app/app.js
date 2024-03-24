d44b31f8e21b077e5b9feb1e2a7f6199578d9b0f
Troy Pernía <troy00pernia@gmail.com>
2024-03-24 14:07:12 -0400

    change app/app.js: add new configurations for cors middleware (line 7-10). change send by json in response of request (line 14,20,24)

--- app/app.js
+++ app/app.js
@@ -4,21 +4,24 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
 app.use(morgan('dev'))
 
 app.get('/', async (req, res) => {
-    res.status(200).send('Esto es una api para las cookies!')
+    res.status(200).json('Esto es una api para las cookies!')
 })
 
 app.get('/cookie', async (req, res) => {
     const cookie = "myCookie=galleta"
     res.setHeader('Set-Cookie', cookie)
-    res.status(200).send('Acabas de solicitar una cookie')
+    res.status(200).json('Acabas de solicitar una cookie')
 })
 
 app.get('/noCookie', async(req,res) => {
-    res.status(200).send('Esta es una ruta libre de cookies')
+    res.status(200).json('Esta es una ruta libre de cookies')
 })
 
 module.exports = app
