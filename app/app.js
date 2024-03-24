9bb200ff5139880969192234091a2c041454d535
Troy Pernía <troy00pernia@gmail.com>
2024-03-24 19:24:11 -0400

    verificar si pasan cookies al servidor

--- app/app.js
+++ app/app.js
@@ -13,6 +13,10 @@ const app = express()
 //     credentials: true
 // }))
 app.use((req, res, next) => {
+    console.log('cookie:', req.headers.cookie)
+    next()
+})
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', `${PATH_FRONT}`)
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
